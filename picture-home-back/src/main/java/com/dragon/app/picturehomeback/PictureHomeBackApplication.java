588eff3732defe536f881cd6be77f89ecb957c81
DragonWuuu <3350986682@qq.com>
2025-11-28 15:13:33 +0800

    init:初始化 spring boot 后端项目

---
+++ picture-home-back/src/main/java/com/dragon/app/picturehomeback/PictureHomeBackApplication.java
@@ -0,0 +1,13 @@
+package com.dragon.app.picturehomeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureHomeBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PictureHomeBackApplication.class, args);
+    }
+
+}
---
+++ picture-home-back/src/test/java/com/dragon/app/picturehomeback/PictureHomeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragon.app.picturehomeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictureHomeBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
